3264429bade564f5d422920992a2e8781694d3db
Jacob Sanders <j.q.sanders@gmail.com>
2018-04-23 11:13:55 -0400

    adds better errors, deletes unnecessary param

--- scripts/unknowns_to_sqlite_loader.py
+++ scripts/unknowns_to_sqlite_loader.py
@@ -1,6 +1,5 @@
 #!usr/bin/env python3
 
-import sys
 import sqlite3
 import csv
 from collections import namedtuple
@@ -18,17 +17,21 @@ Connection = namedtuple(
 
 class SQLiteLoader:
 
-    def __init__(self, infile, target_db, tablename=None):
+    def __init__(self, infile, target_db, tablename):
         self.infile = infile
         self.db = target_db
         self.tablename = tablename
+        self.__missing_db_error = 'OH JEEZ: Seems like the target_db parameter isn\'t set correctly. Without those, we can\'t make a database table.'
+        self.__missing_db_params_error = 'GOLLY: Not seeing either headers or a tablename present. Without those, we can\'t make a database table.'
+
+        if not self.tablename or not self.db:
+            raise Exception(self.__missing_db_error)
 
     def prepare_csv_data(self, filename, has_headers=False):
         with open(filename) as f:
             reader = csv.reader(f, delimiter=',', quotechar='"')
             if has_headers:
                 headers = tuple(next(reader))
-                # headers = None
             else:
                 headers = tuple()
 
@@ -44,14 +47,14 @@ class SQLiteLoader:
             db.cursor()
         )
 
-    def write_data_to_sqlite(self, data, headers, tablename, db_connection):
+    def load_data(self, data, headers, tablename, db_connection):
         try:
             db_connection.cur.execute(
                 'CREATE TABLE IF NOT EXISTS {tablename} {headers}'
                 .format(tablename=tablename, headers=headers)
             )
         except sqlite3.OperationalError:
-            sys.exit('OH NO: We seem to be missing headers for this dataset or a name for the table. No headers or no name, no db table.')
+            raise Exception(self.__missing_db_params_error)
 
         header_count = len(headers)
         sql = 'INSERT INTO {tablename} VALUES('.format(tablename=tablename) + ','.join(header_count * ['?']) + ')'
@@ -59,13 +62,13 @@ class SQLiteLoader:
         db_connection.db.commit()
         db_connection.db.close()
 
-    def run_the_jewels(self, filename, target_db, tablename, has_headers):
+    def run_the_jewels(self, filename, target_db, tablename):
         data = self.prepare_csv_data(filename, has_headers)
-        if data.headers:
+        if data.headers and tablename:
             connection = self.connect_to_db(target_db)
-            self.write_data_to_sqlite(data.rows, data.headers, tablename, connection)
+            self.load_data(data.rows, data.headers, tablename, connection)
         else:
-            sys.exit('YIKES: Not seeing any headers present. Without headers, we can\'t make a database table')
+            raise Exception(self.__missing_db_params_error)
 
 
 if __name__ == '__main__':
@@ -73,4 +76,4 @@ if __name__ == '__main__':
     target_db = 'sqlite/cmoa_unknowns.db'
     tablename = 'unknowns_from_anonymous'
     loader = SQLiteLoader(filename, target_db, tablename)
-    loader.run_the_jewels(loader.infile, loader.db, loader.tablename, True)
+    loader.run_the_jewels(loader.infile, loader.db, loader.tablename)
